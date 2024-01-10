a359dd760126291462ae7043e410427b1ededba7
Thiago Vilar <oceanocabral@gmail.com>
2024-01-10 12:18:51 -0300

    Desafio concluído! (att versão .NET8.0 - alteração em .csproj)

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -2,23 +2,27 @@ namespace DesafioPOO.Models
 {
     public abstract class Smartphone
     {
-        public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        public string Numero { get; private set; }
+        protected string Modelo { get; set; }
+        protected string IMEI { get; set; }
+        protected int Memoria { get; set; }
 
-        public Smartphone(string numero)
+        protected Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            IMEI = imei;
+            Memoria = memoria;
         }
 
         public void Ligar()
         {
-            Console.WriteLine("Ligando...");
+            Console.WriteLine($"Smartphone \n{Modelo} ligando...");
         }
 
         public void ReceberLigacao()
         {
-            Console.WriteLine("Recebendo ligação...");
+            Console.WriteLine($"Smartphone \n{Modelo} recebendo ligação...");
         }
 
         public abstract void InstalarAplicativo(string nomeApp);
--- Program.cs
+++ Program.cs
@@ -1,3 +1,20 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+    Console.WriteLine("\n");
+    Nokia nokia = new Nokia("123456789", "Nokia Lumia", "IMEI123456789", 512);
+    nokia.Ligar();
+    nokia.InstalarAplicativo("WhatsApp");
+
+    Console.WriteLine("\n");
+    Iphone iphone = new Iphone("987654321", "iPhone 13", "IMEI987654321", 2048);
+    iphone.ReceberLigacao();
+    iphone.InstalarAplicativo("Telegram");
+    Console.WriteLine("\n");
+
+//implementar (sequência bash)
+//git config --global user.name "Thiago Vilar"
+//git config --global user.email "oceanocabral@gmail.com"
+//git status
+//git add .
+//git commit -m "Desafio concluído(att versão .NET8.0-alteração .csproj)"
+//git push
